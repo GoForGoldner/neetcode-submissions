4e77a49d85133f33610ff49ea282058859921256
GoForGoldner <71520146+GoForGoldner@users.noreply.github.com>
2026-06-07 09:44:32 -0400

    Add: missing-number - submission-2

---
+++ Data Structures & Algorithms/missing-number/submission-2.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+
+        // The numbers always range from 0 to nums.length
+        // Cyclic sort
+        for (int i = 0; i < n; i++) {
+            while (nums[i] < n && nums[i] != nums[nums[i]]) {
+                // Skip swapping elements that aren't in range
+                if (nums[i] < 0 || nums[i] >= n) continue;
+
+                // Swap the two numbers
+                // int temp = nums[i];
+                // nums[i] = nums[nums[i]];
+                // nums[nums[i]] = temp;
+
+                int target = nums[i];
+                int temp = nums[i];
+                nums[i] = nums[target];
+                nums[target] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i) return i;
+        }
+
+        return n;
+    }
+}
