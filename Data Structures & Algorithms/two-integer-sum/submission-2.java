3b2e397c91bc2e5dc9459e8b8ea6240df3700dd1
GoForGoldner <71520146+GoForGoldner@users.noreply.github.com>
2026-08-21 11:34:20 -0400

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+
+        Map<Integer, Integer> numToIndex = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            int compliment = target - nums[i];
+            
+            if (numToIndex.containsKey(compliment)) {
+                return new int[]{ numToIndex.get(compliment), i };
+            } else {
+                numToIndex.put(nums[i], i);
+            }
+        }
+
+        // Isn't possible to reach
+        return new int[]{};
+    }
+}
