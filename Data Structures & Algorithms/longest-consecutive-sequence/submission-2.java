d870545ff9191e57ae300e98b18253e1cfc22699
GoForGoldner <71520146+GoForGoldner@users.noreply.github.com>
2026-08-21 12:52:53 -0400

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.java
@@ -0,0 +1,33 @@
+class Solution {
+    // [2,20,4,10,3,4,5]
+    // Sorted: [2,3,4,4,5,10,20]
+    public int longestConsecutive(int[] nums) {
+        if (nums.length == 0) return 0;
+
+        int n = nums.length;
+
+        // Sort the array (ascending order)
+        Arrays.sort(nums);
+
+        int max = 0;
+        int count = 0;
+
+        int prev = nums[0];
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == prev) {
+                continue;
+            } else if (nums[i] == prev + 1) {
+                count++;
+            } else {
+                max = Math.max(max, count);
+                count = 0;
+            }
+
+            prev = nums[i];
+        }
+
+        max = Math.max(max, count);
+
+        return max + 1;
+    }
+}
