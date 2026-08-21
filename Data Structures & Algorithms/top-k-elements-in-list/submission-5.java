4191be4be82881b3f96fe9270c1c461e61788df3
GoForGoldner <71520146+GoForGoldner@users.noreply.github.com>
2026-08-21 11:17:30 -0400

    Add: top-k-elements-in-list - submission-5

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-5.java
@@ -0,0 +1,40 @@
+class Solution {
+    // Questions I don't get. 
+    // 1. Why is the size of the freq list n + 1?
+    // The size of nums in the question is from -1000 to 1000 so I'm confused.
+
+    // 2. why does the index < k have to be inside the for loop? 
+    // Won't it just break when index == k?
+
+    public int[] topKFrequent(int[] nums, int k) {
+        int n = nums.length;
+
+        // Create frequency list
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        // Create bucket list from the freq list
+        List<Integer>[] freq = new List[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            freq[i] = new ArrayList<>();
+        }
+
+        // Populate the bucket
+        for (Map.Entry<Integer, Integer> entry : freqMap.entrySet()) {
+            freq[entry.getValue()].add(entry.getKey());
+        }
+
+        int[] output = new int[k];
+        int index = 0;
+        for (int i = n; i > 0; i--) {
+            for (int num : freq[i]) {
+                output[index++] = num;
+                if (index == k) return output;
+            }
+        }
+
+        return output;
+    }
+}
