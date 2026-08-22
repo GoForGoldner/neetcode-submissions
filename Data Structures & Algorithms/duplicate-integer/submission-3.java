d9cd9bd5a9cf165be8e4670427a51c9285e3740a
GoForGoldner <71520146+GoForGoldner@users.noreply.github.com>
2026-08-22 19:04:28 -0400

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> numbers = new HashSet<>();
+        
+        for (int n : nums) {
+            if (numbers.contains(n)) return true;
+            numbers.add(n);
+        }
+
+        return false;
+    }
+}
