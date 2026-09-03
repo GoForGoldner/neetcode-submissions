a9333257b56b0eed9b6767285170f82d2cf877bc
GoForGoldner <71520146+GoForGoldner@users.noreply.github.com>
2026-09-03 14:08:09 -0400

    Add: longest-consecutive-sequence - submission-9

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-9.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        // Create a map that tracks keeps the left and right ends always updated for the longest sequence
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        int max = 0;
+
+        for (int num : nums) {
+            if (map.containsKey(num)) continue;
+
+            int left = map.getOrDefault(num - 1, 0);
+            int right = map.getOrDefault(num + 1, 0);
+            int updatedValue = left + right + 1;
+
+            // Update left and right endpoints based on current sequence
+            map.put(num, updatedValue);
+            map.put(num - left, updatedValue);
+            map.put(num + right, updatedValue);
+            max = Math.max(max, updatedValue);
+        }
+
+        return max;
+    }
+}
